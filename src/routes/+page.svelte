4a000393e3ce9303dd91ad8da7a9d0f040efa086
TRUENO <trueno3954@gmail.com>
2024-01-21 19:14:04 +0900

    add a backgroundimages and change style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,4 +27,9 @@
     text-align: center;
     font-family: "Noto Sans JP", sans-serif;
   }
+  :root {
+    background-image: url(./images/2_20240121190530_1.png);
+    background-size: 25%;
+  }
+
 </style>
